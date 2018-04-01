45eb9890d2e47d5bdcd113eea4fe17de020f6a47
Ton <ewerton.pf@gmail.com>
2018-04-01 17:06:05 -0300

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         
         #Hardcode Temporario 
-        @article.user = User.first();
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfuly created.."
             redirect_to article_path(@article)
